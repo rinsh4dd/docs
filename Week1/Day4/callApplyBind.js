e046abbe6cd8d916cb61725f3504b4c6f99c6b6d
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-09 16:12:55 +0530

    call-apply-bind

---
+++ Week1/Day4/callApplyBind.js
@@ -0,0 +1,31 @@
+let name = {
+    firstName: "Mohammed",
+    lastName: "Rinshad",
+
+}
+let printName = function (state, pin) {
+    console.log(this.firstName + " " + this.lastName + "  " + state + " ," + pin)
+}
+
+let name2 = {
+    firstName: "Dewald",
+    lastName: "Brevis"
+}
+let name3 = {
+    firstName: "sachin",
+    lastName: "Tendulkar"
+}
+
+//call 
+printName.call(name2, "Mumbai", 979556)
+
+
+//apply
+printName.apply(name, ["kerala", 676551])
+
+
+//bind
+let printMyName = printName.bind(name3,"maharashtra",955995)
+printMyName()
+
+
