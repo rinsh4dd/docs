b1c4efc3fafbd847239856df08a1835ce227425a
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-10 14:33:50 +0530

    docs

--- Week1/Day4/map.js
+++ Week1/Day4/map.js
@@ -1,5 +1,7 @@
 // const { get } = require("http");
 
+const { get } = require("http");
+
 // let map = new Map();
 // map.set("Rinshad", "javascript")
 // map.set("arsha", "CVT")
@@ -18,13 +20,16 @@
 
 let myMap= new Map();
 let anotherMap= new Map([
-    ['name','Rinshad'],
+    ['qualification',function qua(){
+        return "bca"
+    }],
     ['place','malappuram'],
     ['age',20]
 ])
+console.log('hello' + t)
 
 anotherMap.delete('name')
 anotherMap.set('nameAfterDelete',"Rinshad")
 anotherMap.set("age",21)
-console.log(anotherMap)
+console.log(anotherMap.get('qualification')())
 anotherMap.forEach((x,y)=>{console.log(y," : " ,x)})
