dfdbf346fccb8d8409001673a9ca6f8577fe88a6
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-18 14:09:50 +0530

    docs

---
+++ Week2/Day4/currying.js
@@ -0,0 +1,44 @@
+// function multiply(a,b){
+// console.log(a*b)
+// }
+
+const { count } = require("console");
+
+
+// let multiplyByTwo = multiply.bind(null,2)
+// multiplyByTwo(3)
+
+
+// let multiplyByThree= multiply.bind(undefined,3)
+// multiplyByThree(10)
+// let name='Rinshad';
+// function greet(name, callback) {
+//     console.log("Hai" + " " + name)
+//     callback()
+// }
+
+// let Saybye = function(){
+//     console.log(name+ " "+ "Bye")
+// }
+// greet("Rinshad", Saybye)
+
+
+
+// function sum(a,b){
+//     return (a+b)
+// }
+// console.log(sum(10,20))
+
+
+// function sum(a){
+//   return  function(b){
+//     return a+b
+//   }
+// }
+// const add2=sum(10)
+// console.log(add2(5))
+
+
+// const sum=a=>b=>c=>a+b+c
+
+// console.log(sum(10)(15)(10))
--- Week2/Day4/generator.js
+++ Week2/Day4/generator.js
@@ -2,7 +2,10 @@
 //     yield "hello";
 //     yield"Rinshad";
 //     yield"How are you";
-    
+
+const { error } = require("console");
+const { response } = require("express");
+
 // }
 // const greeter=greet()
 // console.log(greeter.next())
@@ -11,24 +14,37 @@
 
 
 
-function* funcGen(){
-    let i=1;
-    while(i<=5){
-        yield i;
-        i++
-    }
-}
+// function* funcGen(){
+//     let i=1;
+//     while(i<=5){
+//         yield i;
+//         i++
+//     }
+// }
 
-const gen=funcGen();
+// const gen=funcGen();
 
-console.log(gen.next())
+// console.log(gen.next())
 
-console.log(gen.next())
+// console.log(gen.next())
 
-console.log(gen.next())
+// console.log(gen.next())
 
-console.log(gen.next())
+// console.log(gen.next())
 
-console.log(gen.next())
+// console.log(gen.next())
 
-console.log(gen.next())
+// console.log(gen.next())
+
+
+
+
+// function*generatorFunc(){
+//     yield "HELLO",
+//     yield "World"
+// }
+// const generate=generatorFunc()
+
+// console.log(generate.next())
+// console.log(generate.next())
+// console.log(generate.next())
